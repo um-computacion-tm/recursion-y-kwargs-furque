2c38a81f38a071de38c67ff157fab0e273b002f3
furque <joaquinfurque8@gmail.com>
2024-04-22 14:44:05 -0300

    Update Buscar_Datos.py

--- Buscar_Datos.py
+++ Buscar_Datos.py
@@ -29,7 +29,14 @@ database = {
         'nombre3': 'Gonzalo',
         'apellido1': 'Messi',
         'apellido2': 'Roccuzo'
-    }
+    },
+    '4': {
+        'nombre1': 'Sebastian',
+        'nombre2': 'Matias',
+        'apellido1': 'Merino',
+        'apellido2': 'Roldan'
+    }    
+    
 }
 
 print(buscar_datos("Pablo", "Diego", "Ruiz", "Picasso", **database))
@@ -37,17 +44,37 @@ print(buscar_datos("Joaquin", "Furque", **database))
 print(buscar_datos("Lionel", "Andres", "Gonzalo", "Messi", "Roccuzo", **database))
 
 class TestPersona(unittest.TestCase):
-    def test_persona1(self):
+    def test_1(self):
         resultado = buscar_datos("Pablo", "Diego", "Ruiz", "Picasso", **database)
         self.assertEqual(resultado, "1")
     
-    def test_persona2(self):
+    def test_2(self):
         resultado = buscar_datos("Joaquin", "Furque", **database)
         self.assertEqual(resultado, "2")
     
-    def test_persona3(self):
+    def test_3(self):
         resultado = buscar_datos("Lionel", "Andres", "Gonzalo", "Messi", "Roccuzo", **database)
         self.assertEqual(resultado, "3")
+
+    def test_3(self):
+        resultado = buscar_datos("Lionel", "Miguel", "Gonzalo", "Messi", "Roccuzo", **database)
+        self.assertEqual(resultado, "No existe")
+
+    def test_1(self):
+        resultado = buscar_datos("Pablo", "Diego", "Ruiz", "Martinez", **database)
+        self.assertEqual(resultado, "No existe")
+
+    def test_4(self):
+        resultado = buscar_datos("Sebastian", "Matias", "Merino", "Roldan", **database)
+        self.assertEqual(resultado, "4")       
+    
+    def test_4(self):
+        resultado = buscar_datos("Sebastian", "Roberto", "Merino", "Roldan", **database)
+        self.assertEqual(resultado, "No existe") 
+    
+    def test_2(self):
+        resultado = buscar_datos("Joaquin", "Ñurfle", **database)
+        self.assertEqual(resultado, "No existe") 
 
 
 unittest.main()
