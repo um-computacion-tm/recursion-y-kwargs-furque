fb5180d9a310480f06876f40548594189de5d730
furque <joaquinfurque8@gmail.com>
2024-04-23 08:45:41 -0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,14 @@
 import unittest
+
 def fibonacci(n):
-    #Suerte!
-    ...
+    if n==0:
+        return 0
+    if n==1:
+        return 1 
+    resultado=fibonacci(n-1)+fibonacci(n-2)
+    return resultado
     
+
 class TestFibonacci(unittest.TestCase):
     def test_with_0(self):
         resultado = fibonacci(0)
@@ -14,11 +20,7 @@ class TestFibonacci(unittest.TestCase):
     
     def test_with_2(self):
         resultado = fibonacci(2)
-        self.assertEqual(resultado, 2)
-    
-    def test_with_2(self):
-        resultado = fibonacci(2)
-        self.assertEqual(resultado, 2)
+        self.assertEqual(resultado, 1)
     
     def test_with_5(self):
         resultado = fibonacci(5)
@@ -27,6 +29,18 @@ class TestFibonacci(unittest.TestCase):
     def test_with_6(self):
         resultado = fibonacci(6)
         self.assertEqual(resultado, 8)
+        
+    def test_with_7(self):
+        resultado = fibonacci(7)
+        self.assertEqual(resultado, 13)
+
+    def test_with_8(self):
+        resultado = fibonacci(8)
+        self.assertEqual(resultado, 21)
+
+    def test_with_15(self):
+        resultado = fibonacci(15)
+        self.assertEqual(resultado, 610)
     
 
 
